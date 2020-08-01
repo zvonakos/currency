4671ed1b2013043357163576b2e8e6aa96991459
mac <mac@MacBook-Pro-Kostya.local>
2020-08-02 00:33:35 +0300

    regex commit

---
+++ Regular_ex/parse_regex.py
@@ -0,0 +1,23 @@
+import re
+import csv
+
+PATH = './nginx.log'
+
+regex = re.compile(
+    r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*\[(?P<date>.*)\].*  \"'
+    r'(?:POST|GET|DELETE|PUT|HEAD|OPTIONS|CONNECT)\s(.*?)\sHTTP\/\d\.\d'
+)
+
+with open(PATH) as file:
+    with open('nginx_regex.csv', 'w', newline='') as csv_file:
+        data_writer = csv.writer(csv_file, delimiter=',')
+        for index, line in enumerate(file):
+            res = regex.search(line)
+            if res is not None:
+                res = res.groupdict()
+
+                ip = res['ip']
+                date = res['date']
+                method = res['POST|GET|DELETE|PUT|HEAD|OPTIONS|CONNECT']
+                path = '/' + res['HTTP']
+                data_writer.writerow([ip, date, method, path])
